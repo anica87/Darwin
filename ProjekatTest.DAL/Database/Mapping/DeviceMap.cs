3584b1410bbb445f362efbcaf2ec81dd259382f4
anica <anica20@gmail.com>
2015-09-02 10:20:30 +0200

    Added a missed DeviceMap class.

---
+++ ProjekatTest.DAL/Database/Mapping/DeviceMap.cs
@@ -0,0 +1,14 @@
+﻿using FluentNHibernate.Mapping;
+using ProjekatTest.Infrastructure.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjekatTest.DAL.Database.Mapping
+{
+   public class DeviceMap : ClassMap<Device>
+    {
+    }
+}
